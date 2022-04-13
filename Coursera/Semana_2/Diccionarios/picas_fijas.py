870c48cea7de1c277b11b7621f0128249feeccb6
Luis <luisfernandosantaospina@gmail.com>
2022-04-13 09:22:21 -0500

    Agrega ejercicio aparicion de digitos

---
+++ Coursera/Semana_2/Diccionarios/picas_fijas.py
@@ -0,0 +1,16 @@
+# El juego de las Picas y Fijas es un juego matemático muy sencillo, consiste en adivinar un número de 4
+# cifras y de todos los dígitos diferentes. Para esto, el jugador que intenta adivinar deberá decir el
+# número que cree está escondiendo el otro, y este deberá responder el número de picas y fijas que tiene
+# ahora el jugador.
+#
+# Una pica es un dígito que se encuentra en el número a adivinar, pero no está en el lugar correcto;
+# y una fija es un dígito correctamente colocado.
+#
+# Por ejemplo, si el número secreto es 1234 y el otro jugador dice 1325, tendrá dos picas y una fija.
+#
+# Debes crear una función que devuelva un diccionario con las llaves "PICAS" y "FIJAS" que represente
+# el resultado de la jugada si un jugador trata de adivinar el numero_secreto con el número intento.
+#
+# Tu solución debe tener una función de acuerdo con la siguiente especificación:
+# Nombre de la función: picas_y_fijas
+# Si lo requieres, puedes agregar funciones adicionales.
