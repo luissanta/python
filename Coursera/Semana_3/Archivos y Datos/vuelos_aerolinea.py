430eb43ee7d357304f23066d025b404f34b18d28
Luis <luisfernandosantaospina@gmail.com>
2022-04-22 08:24:19 -0500

    Agrega ejercicio de mejor aerolinea

--- Coursera/Semana_3/Archivos y Datos/vuelos_aerolinea.py
+++ Coursera/Semana_3/Archivos y Datos/vuelos_aerolinea.py
@@ -3,9 +3,14 @@
 # Nuestra función recibirá como parámetro el diccionario de vuelos y el código
 # de un aeropuerto, y retornará a una lista con los códigos de los vuelos.
 
+
 # La segunda función que implementaremos nos permite conocer el código del vuelo
 # más largo, es decir, el de mayor duración de una aerolínea. Para este caso, necesitamos
 # que la función reciba por parámetro el diccionario de vuelos y el código de una de las aerolíneas
+
+
+# En este se nos pide implementar una función que permita conocer el código del aeropuerto más visitado,
+# teniendo en cuenta tanto los vuelos que salen de éste, como los que llegan.
 
 
 def lee_vuelos() -> dict:
@@ -46,10 +51,25 @@ def vuelos_mas_largo_por_aerolinea(lista_vuelos: dict, codigo_aerolinea: str) ->
     return vuelo_mas_largo
 
 
+def aeropuerto_mas_visitado(lista_vuelos: dict) -> str:
+    vuelos_visitas = {}
+    for vuelo in lista_vuelos.values():
+        origen = vuelo['origen']
+        destino = vuelo['destino']
+        vuelos_visitas[origen] = vuelos_visitas.get(origen, 0) + 1
+        vuelos_visitas[destino] = vuelos_visitas.get(destino, 0) + 1
+
+    mas_visitas = max(vuelos_visitas.values())
+    aeropuerto_mas_visitas = list(vuelos_visitas.keys())[list(vuelos_visitas.values()).index(mas_visitas)]
+    return aeropuerto_mas_visitas
+
+
 aeropuerto_a_buscar_vuelos = 'YUU'
-print('Los vuelos con origen del aeropuerto', aeropuerto_a_buscar_vuelos, 'son: ',
+print('Los vuelos con origen del aeropuerto', aeropuerto_a_buscar_vuelos, 'son:',
       vuelos_por_aeropuerto(lee_vuelos(), aeropuerto_a_buscar_vuelos))
 
 aerolinea_a_buscar_vuelo_largo = 'QWE'
-print('El vuelo mas largo de la aerolínea', aerolinea_a_buscar_vuelo_largo, 'es: ',
+print('El vuelo mas largo de la aerolínea', aerolinea_a_buscar_vuelo_largo, 'es:',
       vuelos_mas_largo_por_aerolinea(lee_vuelos(), aerolinea_a_buscar_vuelo_largo))
+
+print('El aeropuerto mas visitado es:', aeropuerto_mas_visitado(lee_vuelos()))
