6c8bacbf712ba2d3acdf7a34f8c67c92900de5ec
Luis <luisfernandosantaospina@gmail.com>
2022-04-26 18:29:35 -0500

    Finaliza semana 3

--- Coursera/Semana_3/Proyecto/consola_billboard.py
+++ Coursera/Semana_3/Proyecto/consola_billboard.py
@@ -12,8 +12,8 @@ Temas:
 
 @author: Cupi2
 """
-
 import billboard as bb
+
 
 def ejecutar_cargar_canciones() -> list:
     """Solicita al usuario que ingrese el nombre de un archivo CSV con los datos de
@@ -21,89 +21,82 @@ def ejecutar_cargar_canciones() -> list:
     Retorno: list
         La lista de canciones con la información del archivo.
     """
-    canciones = None
     archivo = input("Por favor ingrese el nombre del archivo CSV con las canciones: ")
     canciones = bb.cargar_canciones(archivo)
     if len(canciones) == 0:
         print("El archivo seleccionado no es válido. No se pudieron cargar las canciones del Ranking")
     else:
-        print("Se cargaron", len(canciones), "canciones a partir del archivo.")
+        print("Se cargaron", len(canciones), "canciones a partir del archivo", archivo)
     return canciones
 
-def ejecutar_buscar_cancion(canciones:list)->None:
+
+def ejecutar_buscar_cancion(canciones: list) -> None:
     """ Ejecuta la opción de buscar una canción dado el nombre y el año del 
     ranking al cual pertenece 
     """
     cancion = input("Por favor ingrese el nombre de la canción que desea buscar: ")
     anio = int(input("Por favor ingrese el año de la canción que desea buscar: "))
-    
-    #TODO: complete el código haciendo el llamado a la función del módulo que
-    #implementa este requrimiento e imprimiendo por pantalla el resultado
+    print(bb.buscar_cancion(canciones, cancion, anio))
 
-def ejecutar_canciones_anio(canciones:list)->None:
+
+def ejecutar_buscar_canciones_anio(canciones: list) -> None:
     """ Ejecuta la opción de consultar las canciones de un año dado 
     """
     anio = int(input("Por favor ingrese el año que desea consultar: "))
+    print(bb.buscar_canciones_anio(canciones, anio))
 
-    #TODO: complete el código haciendo el llamado a la función del módulo que
-    #implementa este requrimiento e imprimiendo por pantalla el resultado
-    
-def ejecutar_canciones_artista_periodo(canciones:list)->None:
+
+def ejecutar_buscar_canciones_artista_periodo(canciones: list) -> None:
     """ Ejecuta la opción de consultar las canciones de un artista dado en 
     un periodo de tiempo definido 
     """
     artista = input("Por favor ingrese el nombre del artista que desea buscar: ")
-    anio_inic = int(input("Por favor ingrese el año inicial que desea buscar: "))
+    anio_inicio = int(input("Por favor ingrese el año inicial que desea buscar: "))
     anio_fin = int(input("Por favor ingrese el año final que desea buscar: "))
+    print(bb.buscar_canciones_artista_periodo(canciones, artista, anio_inicio, anio_fin))
 
-    #TODO: complete el código haciendo el llamado a la función del módulo que
-    #implementa este requrimiento e imprimiendo por pantalla el resultado
-    
-def ejecutar_todas_canciones_artista(canciones:list)->None:
+
+def ejecutar_buscar_canciones_artista(canciones: list) -> None:
     """ Ejecuta la opción de consultar todas las canciones de un artista dado 
     """
     artista = input("Por favor ingrese el nombre del artista que desea buscar: ")
+    print(bb.buscar_canciones_artista(canciones, artista))
 
-    #TODO: complete el código haciendo el llamado a la función del módulo que
-    #implementa este requrimiento e imprimiendo por pantalla el resultado
 
-def ejecutar_todos_artistas_cancion(canciones:list)->None:
+def ejecutar_buscar_todos_artistas_cancion(canciones: list) -> None:
     """ Ejecuta la opción de consultar todos los artistas que han interpretado 
     una canción dada 
     """
-    min = input("Por favor ingrese el nombre de la canción que desea buscar: ")
+    cancion = input("Por favor ingrese el nombre de la canción que desea buscar: ")
+    print(bb.buscar_todos_artistas_cancion(canciones, cancion))
 
-    #TODO: complete el código haciendo el llamado a la función del módulo que
-    #implementa este requrimiento e imprimiendo por pantalla el resultado
 
-def ejecutar_artistas_mas_populares(canciones:list)->None:
+def ejecutar_buscar_artistas_mas_populares(canciones: list) -> None:
     """ Ejecuta la opción de consultar los artistas más populares 
     """
-    min = input("Por favor ingrese la cantidad mínima de canciones que desea buscar: ")
+    minimo = int(input("Por favor ingrese la cantidad mínima de canciones que desea buscar: "))
+    print(bb.buscar_artistas_mas_populares(canciones, minimo))
 
-    #TODO: complete el código haciendo el llamado a la función del módulo que
-    #implementa este requrimiento e imprimiendo por pantalla el resultado
-     
-def ejecutar_artista_estrella(canciones:list)->None:
+
+def ejecutar_buscar_artista_estrella(canciones: list) -> None:
     """ Ejecuta la opción de consultar el artista estrella de todos los tiempos 
     """
-    #TODO: complete el código haciendo el llamado a la función del módulo que
-    #implementa este requrimiento e imprimiendo por pantalla el resultado
+    print(bb.buscar_artista_estrella(canciones))
 
-def ejecutar_artistas_y_sus_canciones(canciones:list)->None:
+
+def ejecutar_buscar_artistas_y_sus_canciones(canciones: list) -> None:
     """ Ejecuta la opción de consultar la lista completa de artistas del Billboard 
     junto con sus canciones 
     """
-    #TODO: complete el código haciendo el llamado a la función del módulo que
-    #implementa este requrimiento e imprimiendo por pantalla el resultado
+    print(bb.buscar_artistas_y_sus_canciones(canciones))
 
-def ejecutar_promedio_canciones_por_artista(canciones:list)->None:
+
+def ejecutar_calcula_promedio_canciones_por_artista(canciones: list) -> None:
     """ Ejecuta la opción de consultar la cantidad promedio de canciones que los 
     artistas tienen en el listado de Billboard 
     """
-    #TODO: complete el código haciendo el llamado a la función del módulo que
-    #implementa este requrimiento e imprimiendo por pantalla el resultado
-    
+    print(bb.calcula_promedio_canciones_por_artista(canciones))
+
     
 def mostrar_menu():
     """Imprime las opciones de ejecución disponibles para el usuario.
@@ -121,38 +114,27 @@ def mostrar_menu():
     print("10. Consultar la cantidad promedio de canciones por artista")
     print("11. Salir.")
 
+
 def iniciar_aplicacion():
     """Ejecuta el programa para el usuario."""
     continuar = True
     canciones = list()
     while continuar:
         mostrar_menu()
-        opcion_seleccionada = int(input("Por favor seleccione una opción: "))
-        if opcion_seleccionada == 1:
-            canciones=ejecutar_cargar_canciones()
-        elif opcion_seleccionada == 2:
-            ejecutar_buscar_cancion(canciones)
-        elif opcion_seleccionada == 3:
-            ejecutar_canciones_anio(canciones)
-        elif opcion_seleccionada == 4:
-            ejecutar_canciones_artista_periodo(canciones)
-        elif opcion_seleccionada == 5:
-            ejecutar_todas_canciones_artista(canciones)
-        elif opcion_seleccionada == 6:
-            ejecutar_todos_artistas_cancion(canciones)
-        elif opcion_seleccionada == 7:
-            ejecutar_artistas_mas_populares(canciones)
-        elif opcion_seleccionada == 8:
-            ejecutar_artista_estrella(canciones)
-        elif opcion_seleccionada == 9:
-            ejecutar_artistas_y_sus_canciones(canciones)
-        elif opcion_seleccionada == 10:
-            ejecutar_promedio_canciones_por_artista(canciones)            
-        elif opcion_seleccionada == 11:
-            continuar = False
-        else:
-            print("Por favor seleccione una opción válida.")
+        opcion_seleccionada = input("Por favor seleccione una opción: ")
+        match opcion_seleccionada:
+            case '1': canciones = ejecutar_cargar_canciones()
+            case '2': ejecutar_buscar_cancion(canciones)
+            case '3': ejecutar_buscar_canciones_anio(canciones)
+            case '4': ejecutar_buscar_canciones_artista_periodo(canciones)
+            case '5': ejecutar_buscar_canciones_artista(canciones)
+            case '6': ejecutar_buscar_todos_artistas_cancion(canciones)
+            case '7': ejecutar_buscar_artistas_mas_populares(canciones)
+            case '8': ejecutar_buscar_artista_estrella(canciones)
+            case '9': ejecutar_buscar_artistas_y_sus_canciones(canciones)
+            case '10': ejecutar_calcula_promedio_canciones_por_artista(canciones)
+            case '11': continuar = False
+            case _: print("La opción " + opcion_seleccionada + " no es valida.")
 
-#PROGRAMA PRINCIPAL
+
 iniciar_aplicacion()
-
